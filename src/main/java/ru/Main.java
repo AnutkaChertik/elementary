2cdc5eb58d963df420f0ad9b377c18d852c66edf
AnutkaChertik <ana.ru@mail.ru>
2025-02-01 20:24:44 +0800

    1.3

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -2,6 +2,13 @@ package ru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+       byte age = 50;
+       char gender = 'm';
+       short length = 195;
+       int bricks = 15505;
+       double bacteria = 123456789.524;
+       long distance = 150000000;
+       boolean permission = true;
+       float file = 9577425.715963412450000000000001f;
     }
 }

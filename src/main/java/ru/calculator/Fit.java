3f6f0bb2e4e1476f772456a7ded2e58f9d269755
AnutkaChertik <ana.ru@mail.ru>
2025-02-16 17:49:51 +0800

    2.4

--- src/main/java/ru/calculator/Fit.java
+++ src/main/java/ru/calculator/Fit.java
@@ -2,11 +2,11 @@ package ru.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        return height - 88;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        return height - 101;
+        return (height - 88) * 1.15;
     }
 
     public static void main(String[] args) {

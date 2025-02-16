6923c9cb7d7ad4890d15842b53ed0a9585251019
AnutkaChertik <ana.ru@mail.ru>
2025-02-16 16:20:37 +0800

    2.4

--- src/main/java/ru/calculator/Fit.java
+++ src/main/java/ru/calculator/Fit.java
@@ -2,19 +2,19 @@ package ru.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        return (height - 100) * 1.15;
+        return height - 88;
     }
 
     public static double womanWeight(short height) {
-        return (height - 110) * 1.15;
+        return height - 101;
     }
 
     public static void main(String[] args) {
-        short height = 187;
-        double man = manWeight(height);
-        double woman = womanWeight(height);
-        System.out.println("Man 187 is " + man);
-        System.out.println("Woman 187 is " + woman);
+        short height = 180;
+        double man = Fit.manWeight(height);
+        height = 170;
+        double woman = Fit.womanWeight(height);
+        System.out.println("Man 180 is " + man);
+        System.out.println("Woman 170 is " + woman);
     }
-
 }
